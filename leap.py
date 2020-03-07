06e1d37c8768b3bc65df97c8ecdcc516b453c56b
JGITONGA <jimgitongajm@gmail.com>
2020-03-07 11:41:04 +0300

    Add files via upload

--- leap.py
+++ leap.py
@@ -1,21 +1,19 @@
 
-leap1=[]
-leap2=[]
-for year in range(1800,2400):
 
 
+
+while True:
+  
+  year=int(input("enter the year of your choice: "))
+  
   if (year % 4) == 0:  
     if (year % 100) == 0:  
       if (year % 400) == 0:  
         print(f"{year} is a leap year")
-        leap1.append(year)
       else:  
         print(f"{year} is not a leap year")  
     else:  
       print(f"{year} is a leap year")
-      leap2.append(year)
   else:  
     print(f"{year} is not a leap year")
 
-total=leap2+leap1
-print(len(total))  
